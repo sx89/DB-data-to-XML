6829847dfd8c63f319ef7279080bdbe0364eee92
孙旭 <34908553+sunxu654@users.noreply.github.com>
2019-07-09 10:26:15 +0800

    增加生成xml所用实体类属性

--- src/main/java/com/sx/createxml/pojo/XMLDataStruct/ProcessBasic.java
+++ src/main/java/com/sx/createxml/pojo/XMLDataStruct/ProcessBasic.java
@@ -1,10 +1,48 @@
 package com.sx.createxml.pojo.XMLDataStruct;
 
+import java.util.HashMap;
+
 /**
  * @author sunxu93@163.com
  * @date 19/7/9/009 10:02
  */
 public class ProcessBasic {
+    private String name = "过程行为元数据";
 
-    //TODO  仿照com.sx.createxml.pojo.XMLDataStruct.FileBasic 写
+    public static final String[] listFileBasic = {
+            "行为事件类型",
+            "行为责任人职责",
+            "行为责任者",
+            "行为责任者IP",
+            "行为时间",
+            "行为序号",
+            "处理意见"
+
+    };
+
+    private HashMap<String, Object> hashMap = new HashMap<>();
+
+
+    public ProcessBasic() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static String[] getListFileBasic() {
+        return listFileBasic;
+    }
+
+    public HashMap<String, Object> getHashMap() {
+        return hashMap;
+    }
+
+    public void setHashMap(HashMap<String, Object> hashMap) {
+        this.hashMap = hashMap;
+    }
 }
--- src/main/java/com/sx/createxml/pojo/XMLDataStruct/ServiceBasic.java
+++ src/main/java/com/sx/createxml/pojo/XMLDataStruct/ServiceBasic.java
@@ -1,10 +1,49 @@
 package com.sx.createxml.pojo.XMLDataStruct;
 
+import java.util.HashMap;
+
 /**
  * @author sunxu93@163.com
  * @date 19/7/8/008 18:05
  */
 public class ServiceBasic {
+    private String name = "业务基本信息元数据";
 
-    //TODO  仿照com.sx.createxml.pojo.XMLDataStruct.FileBasic 写
+    public static final String[] listFileBasic = {"设计资质",
+            "子项编号",
+            "专业编号",
+            "归档专业",
+            "文件分类",
+            "文件类别",
+            "文件类别编码",
+            "文件类别序号",
+            "关联文件",
+            "文件是否必归",
+            "文件捕获节点",
+            "文件来源"};
+
+    private HashMap<String, Object> hashMap = new HashMap<>();
+
+    public ServiceBasic() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static String[] getListFileBasic() {
+        return listFileBasic;
+    }
+
+    public HashMap<String, Object> getHashMap() {
+        return hashMap;
+    }
+
+    public void setHashMap(HashMap<String, Object> hashMap) {
+        this.hashMap = hashMap;
+    }
 }
