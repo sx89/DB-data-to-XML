8963012b9db491e87f9459a974ab49d8cbe5beb9
xiaolong-ji <xlj_diling@hotmail.com>
2019-07-11 22:04:50 +0800

    增加与teamcore的通信，以使用gitlab API获得文件数据

--- src/main/java/com/sx/createxml/CreatexmlApplication.java
+++ src/main/java/com/sx/createxml/CreatexmlApplication.java
@@ -2,7 +2,11 @@ package com.sx.createxml;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class CreatexmlApplication {
 
