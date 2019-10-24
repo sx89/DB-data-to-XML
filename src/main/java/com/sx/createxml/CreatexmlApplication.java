e2acb166eb8467b3082ec8a6e781947f07135edd
sx89 <sunxufdu@163.com>
2019-10-24 19:19:32 +0800

    1

--- src/main/java/com/sx/createxml/CreatexmlApplication.java
+++ src/main/java/com/sx/createxml/CreatexmlApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class CreatexmlApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(CreatexmlApplication.class, args);
     }
